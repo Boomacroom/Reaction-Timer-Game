819b10768e3dae12ce242468489756d11e3c4893
Christopher Irick <admin@api.fish>
2021-06-24 20:46:19 -0400

    finished project

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<div class="backdrop" @click.self="cheaterDetected">
+  <h1>Boom's Reaction Timer</h1>
+  <button class="button" @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay='delay' @end="endGame" />
+  <Results :result='score' v-if="showResults"/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false
+    }
+  },
   components: {
-    HelloWorld
+    Block, Results
+  },
+  methods: {
+    start() {
+      this.delay = 1000 + Math.random() * 5000
+      this.isPlaying = true
+      console.log(this.delay)
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    },
+    cheaterDetected() {
+      this.isPlaying = false
+      alert("Cheater Detected! Remember when you cheat, you're only cheating yourself...")
+    }
   }
 }
 </script>
@@ -20,7 +51,51 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
+body{
+  margin: 0;
+}
+
+.button {
+  position: relative;
+  background-color: #4CAF50;
+  border: none;
+  font-size: 28px;
+  color: #FFFFFF;
+  padding: 20px;
+  width: 200px;
+  text-align: center;
+  transition-duration: 0.4s;
+  text-decoration: none;
+  overflow: hidden;
+  cursor: pointer;
+}
+
+.button:after {
+  content: "";
+  background: #f1f1f1;
+  display: block;
+  position: absolute;
+  padding-top: 300%;
+  padding-left: 350%;
+  margin-left: -20px !important;
+  margin-top: -120%;
+  opacity: 0;
+  transition: all 0.8s
+}
+
+.button:active:after {
+  padding: 0;
+  margin: 0;
+  opacity: 1;
+  transition: 0s
+}
+.backdrop {
+    top: 0;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+  }
 </style>
---
+++ src/components/Results.vue
@@ -0,0 +1,36 @@
+<template>
+  <p class='rank'>Reaction Time: {{ result }} ms<br>{{rank}}!</p>
+</template>
+
+<script>
+export default {
+    props: [
+        'result'
+    ],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if (this.result < 250){
+            this.rank = 'Ninja Fingers'
+        } else if (this.result < 350){
+            this.rank = 'Rapid Reflexes'
+        } else if (this.result < 450){
+            this.rank = 'Snails Pace'
+        } else if (this.result > 451){
+            this.rank = 'Check Your Pulse!'
+        } 
+    }
+
+}
+</script>
+
+<style scoped>
+.rank {
+    color: green;
+    font-weight: 600;
+    font-size: 2em;
+}
+</style>
